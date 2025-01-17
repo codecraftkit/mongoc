754b8f0fcf3dfb8ef292892ab4783d3d4a25aebf
Jose G Nieto <jose.nieto@noblenex.us>
2025-01-17 09:56:39 -0500

    mongoc v1.0.0

--- mongoc/mongoc.go
+++ mongoc/mongoc.go
@@ -19,12 +19,6 @@ func Connect(mongoUri string, dbName string, dataStore *MongoDataStore) {
 		panic(err)
 	}
 
-	//defer func() {
-	//	if err = client.Disconnect(context.TODO()); err != nil {
-	//		panic(err)
-	//	}
-	//}()
-
 	// Send a ping to confirm a successful connection
 	var result bson.M
 	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
